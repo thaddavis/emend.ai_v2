026037abe6e075b3b19e1649dc862a41a3e61f33
a <a@a.com>
2023-04-20 05:05:50 -0400

    fix missing border on queryAI modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -75,7 +75,7 @@ export const Modal = (props: P) => {
                   <textarea
                     rows={4}
                     value={queryContent} // ...force the input's value to match the state variable...
-                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-2 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     onChange={(e) => setQueryContent(e.target.value)} // ... and update the state variable on any edits!
                   />
                 </div>
